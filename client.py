e2550fe204ae0ad0898f28797c7ed1f9394fda4e
GaussPrimeWF <27castanedag@wirralgrammarboys.com>
2025-09-15 12:13:31 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,33 @@
+import socket
+import threading
 
+nickname = input("Choose your nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('77.103.10.218', 5000))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
+#84.70.49.170
