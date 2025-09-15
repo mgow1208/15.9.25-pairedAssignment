047bdd45adcfd01e09d77f430c8eb4e710f01a2f
mgow1208 <27franklandc@wirralgrammarboys.com>
2025-09-15 12:12:26 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,59 @@
+# WE COULD NOT GET IT TO WORK SINCE IT SAYS WE NEED TO CHANGE FIREWALL SETTINGS, NEITHER OF US HAVE ADMIN PERMISIIONS
+
+import socket
+import threading
+
+# Set the server host to your local IP address
+host = '77.103.10.218'  # Replace this with your local IP
+port = 55555  # The port for the connection
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+clients = []
+nicknames = []
+
+# Broadcast a message to all connected clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handle communication with each client
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)  # Broadcast the message to all clients
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} has left the chat!'.encode('utf-8'))
+            nicknames.remove(nickname)
+            break
+
+# Accept new client connections
+def receive():
+    print(f"Server is listening on {host}:{port}...")
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('NICK'.encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} joined the chat!".encode('utf-8'))
+        client.send('Connected to the server!'.encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+# Start receiving connections
+receive()
 
